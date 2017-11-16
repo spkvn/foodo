398396b2425996932163b05865d51eca52668837
spkvn <kevinmeagher1@hotmail.com>
2017-11-16 21:38:10 +1100

    import fucking foundation or whatever

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,13 @@ let mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+//
+// mix.js('resources/assets/js/app.js', 'public/js')
+//    .sass('resources/assets/sass/app.scss', 'public/css');
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.sass('resources/assets/sass/foodo.scss', 'public/css');
+mix.combine([
+    'node_modules/jquery/dist/jquery.min.js',
+    'node_modules/foundation-sites/dist/js/foundation.min.js',
+    'resources/js/app.js'
+], 'public/js/vendor-start.js' );
